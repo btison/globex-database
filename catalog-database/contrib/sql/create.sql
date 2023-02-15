054687c47bb9295c82e89c9919d79597a38eed16
Bernard Tison <bernard.tison@gmail.com>
2023-02-15 09:37:59 +0100

    added product categories

--- catalog-database/contrib/sql/create.sql
+++ catalog-database/contrib/sql/create.sql
@@ -9,3 +9,24 @@ ALTER TABLE public.catalog OWNER TO $POSTGRESQL_USER;
 
 ALTER TABLE ONLY public.catalog
     ADD CONSTRAINT catalog_pkey PRIMARY KEY (itemid);
+
+CREATE TABLE category (
+  categoryId bigint NOT NULL,
+  category VARCHAR(255)
+);
+
+ALTER TABLE public.category OWNER TO $POSTGRESQL_USER;
+
+ALTER TABLE ONLY public.category
+    ADD CONSTRAINT category_pkey PRIMARY KEY (categoryId);
+
+
+CREATE table category_catalog (
+    itemId VARCHAR(256) NOT NULL,
+    categoryId bigint NOT NULL
+);
+
+ALTER TABLE public.category_catalog OWNER TO $POSTGRESQL_USER;
+
+ALTER TABLE ONLY public.category_catalog
+    ADD CONSTRAINT category_catalog_pkey PRIMARY KEY (itemId, categoryId);
